a8dc9d1f5949baf09e6123b6bcb39ef636e9f0a3
frxvii <frxvii@gmail.com>
2019-02-14 15:23:04 -0500

    BackUp

---
+++ week3/Assets/Scripts/DotProductCalc.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotProductCalc : MonoBehaviour
+{
+    public Vector3 v1Start;
+    public Vector3 v1End;
+    public Vector3 v2Start;
+    public Vector3 v2End;
+
+    private Vector3 _v1;
+    private Vector3 _v2;
+
+    public float outputOfDotProduct1;
+    
+   
+
+    void Update()
+    {
+        Debug.DrawLine(v1Start, v1End);
+        Debug.DrawLine(v2Start, v2End);
+
+        _v1 = v1End - v1Start;
+        _v2 = v2End - v2Start;
+        
+        _v1 = Vector3.Normalize(_v1);
+        _v2 = Vector3.Normalize(_v2);
+        
+        outputOfDotProduct1 = Vector3.Dot(_v1, _v2);
+        
+    }
+}
