f75ebec169dff800f952d5fb9436445bc07cdd17
frxvii <frxvii@gmail.com>
2019-02-18 00:51:46 -0500

    Camera Zoom out, look at ooi.

--- week3/Assets/Scripts/ThirdPersonCameraController.cs
+++ week3/Assets/Scripts/ThirdPersonCameraController.cs
@@ -11,7 +11,16 @@ public class ThirdPersonCameraController : MonoBehaviour {
     private Transform _cameraTransform;
     private Transform _cameraLookTarget;
     private Transform _avatarTransform;
+    private Transform _ObjectOfInterest;
+    private Transform _ST01;
+    private Transform _ST02;
+    private Transform _ST03;
+    private Transform OOI;
     private Rigidbody _avatarRigidbody;
+    
+    private Vector3 CamChaDis;
+    private Vector3 CamObjDis;
+    private float outputOfDotProduct1;
     #endregion
 
     #region Public Tuning Variables
@@ -47,7 +56,10 @@ public class ThirdPersonCameraController : MonoBehaviour {
         _cameraBaseTransform = _view.Find("CameraBase");
         _cameraTransform = _cameraBaseTransform.Find("Camera");
         _cameraLookTarget = _cameraBaseTransform.Find("CameraLookTarget");
-
+        _ObjectOfInterest = _view.Find("ObjectOfInterest");
+        _ST01 = _ObjectOfInterest.Find("ShinyThings01");
+        _ST02 = _ObjectOfInterest.Find("ShinyThings02");
+        _ST03 = _ObjectOfInterest.Find("ShinyThings03");
         _avatarTransform = _view.Find("AIThirdPersonController");
         _avatarRigidbody = _avatarTransform.GetComponent<Rigidbody>();
         idleTime = 0f;
@@ -56,15 +68,17 @@ public class ThirdPersonCameraController : MonoBehaviour {
 
     private void Update()
     {
+        
         if (_Helper_IsIdle())
             _IdleUpdate();
         else if (Input.GetMouseButton(1))
             _ManualUpdate();
         else
             _AutoUpdate();
-
+        print(_Help_IsThereOOI());
         
-        print(_Helper_IsIdle());
+        
+        
         
         
         
@@ -79,16 +93,23 @@ public class ThirdPersonCameraController : MonoBehaviour {
         _ComputeData();
         _FollowAvatar();
         _LookAtAvatar();
+        
     }
     private void _ManualUpdate()
     {
-        _FollowAvatar();
-        _ManualControl();
+        //_FollowAvatar();
+         
+        _currentPosManual = _avatarTransform.position;
+        if (_lastPosManual != _currentPosManual)
+            _ManualControl();
+        else 
+            _ManualCameraRotate();
+        _lastPosManual = _currentPosManual;
     }
     private void _IdleUpdate()
     {
         _ComputeData();
-        
+        _LookAtObject();
         //_ManualControl();
     }
     #endregion
@@ -119,9 +140,40 @@ public class ThirdPersonCameraController : MonoBehaviour {
         _followDistance_Applied = Mathf.Lerp(_followDistance_Standing, _followDistance_Walking, _standingToWalkingSlider);
         _verticalOffset_Applied = Mathf.Lerp(_verticalOffset_Standing, _verticalOffset_Walking, _standingToWalkingSlider);
 
+        if(_Help_IsThereOOI())
+        {
+            float st01dis = Vector3.Distance(_ST01.position,_avatarTransform.position);
+            float st02dis = Vector3.Distance(_ST02.position,_avatarTransform.position);
+            float st03dis = Vector3.Distance(_ST03.position,_avatarTransform.position);
+            
+            if(st01dis<st02dis)
+            {
+                if(st01dis<st03dis)
+                    
+                    OOI = _ST01;
+                    
+                else
+                    OOI = _ST03;
+            }
+            else
+            {
+                if(st02dis<st03dis)
+                    
+                    OOI = _ST02;
+                    
+                else
+                    OOI = _ST03;
+            }
+            
+            //CamChaDis = _avatarTransform.position - OoiDetect.position;
+            //CamObjDis = OOI.position - _cameraTransform.position;
+            //CamChaDis = Vector3.Normalize(CamChaDis);
+            //CamObjDis = Vector3.Normalize(CamObjDis);
+            //outputOfDotProduct1 = Vector3.Dot(CamChaDis,CamObjDis);
+        }
+            
         
     }
-
     private void _FollowAvatar()
     {
         _camRelativePostion_Auto = _avatarTransform.position;
@@ -135,31 +187,42 @@ public class ThirdPersonCameraController : MonoBehaviour {
         _cameraTransform.LookAt(_cameraLookTarget);
     }
 
-    private void _LookAtObject(Transform oOI)
+    private void _LookAtObject()
+    //if the angle between character and object is larger than 80 degrees, the camera zoom out
+    //if the angle is smaller than 80, camera rotate to the object
     {
-        //_cameraTransform.LookAt(oOI);
-
-        Collider[] colliders =
-            Physics.OverlapSphere(_avatarTransform.position, _searchRadius = 5, LayerMask.NameToLayer("ObjectOfInterest"));
-        if (colliders.Length <= 0)
-            return ;
-
-        for (int i = 0; i < colliders.Length; i++)
-            print(colliders[i].gameObject.name);
+        if (_Help_IsThereOOI())
+            if (Vector3.Angle(_avatarTransform.forward, OOI.position - _avatarTransform.position) <= 80f)
+                {
+                _cameraLookTarget.position = Vector3.MoveTowards(_cameraLookTarget.position, OOI.position, Time.deltaTime);
+                _cameraTransform.LookAt(_cameraLookTarget);
+                }
+            else
+                {
+                _cameraTransform.position = 
+                    Vector3.MoveTowards(_cameraTransform.position, _avatarTransform.position - _avatarLookForward * _followDistance_Applied * 1.5f + Vector3.up * _verticalOffset_Applied * 3f, Time.deltaTime);
+                _cameraTransform.LookAt(_cameraLookTarget);
+                
+                }
+            //Debug.Log(Vector3.Angle(_avatarTransform.forward, OOI.position - _avatarTransform.position));
+        
     }
-    private void _ManualControl()
+    private void _ManualCameraRotate()
     {
+        
         //transform.RotateAround : move the axis to target.postion, then rotate along this axis.
         //characters barely rotate vertically, so the world axis if fine in y axis.
-        _cameraTransform.transform.RotateAround (_avatarTransform.position, Vector3.up, Input.GetAxis("Mouse X"));
+        
+        _cameraTransform.RotateAround (_avatarTransform.position, Vector3.up, Input.GetAxis("Mouse X"));
         //because target's local x axis changes, after the character moves. (when a character moves, its face direction rotates)
         //use local instead of world axis.
-        _cameraTransform.transform.RotateAround (_avatarTransform.position, _cameraTransform.transform.right, -1f * Input.GetAxis("Mouse Y"));
-
+        
+        _cameraTransform.RotateAround (_avatarTransform.position, _cameraTransform.transform.right, -1f * Input.GetAxis("Mouse Y"));
+        
     }
     
     
-    /*private void _ManualControl()
+    private void _ManualControl()
     {
         Vector3 _camEulerHold = _cameraTransform.localEulerAngles;
         
@@ -179,10 +242,10 @@ public class ThirdPersonCameraController : MonoBehaviour {
             _camEulerHold.x = temp;
         }
 
-        Debug.Log("The V3 to be applied is " + _camEulerHold);
+        //Debug.Log("The V3 to be applied is " + _camEulerHold);
         _cameraTransform.localRotation = Quaternion.Euler(_camEulerHold);
         
-    }*/
+    }
     #endregion
 
     #region Helper Functions
@@ -192,6 +255,8 @@ public class ThirdPersonCameraController : MonoBehaviour {
     private Transform _Helper_WhatIsClosest;
     private Vector3 lastWalk;
     private Vector3 currentWalk;
+    private Vector3 _lastPosManual;
+    private Vector3 _currentPosManual;
     
 
     private bool _Helper_IsIdle()
@@ -216,13 +281,14 @@ public class ThirdPersonCameraController : MonoBehaviour {
         float velInst = Vector3.Distance(lastWalk, currentWalk) / Time.deltaTime;
         //lastWalk = currentWalk;
         if (velInst > 0.15f)
-            return false;
-        else return true;
+            return true;
+        else return false;
     }
 
     private bool _Help_IsThereOOI()
     {
-        Collider[] stuffInSphere = Physics.OverlapSphere(_avatarTransform.position, 5);
+        Collider[] stuffInSphere = 
+            Physics.OverlapSphere(_avatarTransform.position, _searchRadius = 20);
 
         bool _oOIPresent = false;
 
@@ -234,6 +300,7 @@ public class ThirdPersonCameraController : MonoBehaviour {
 
         return _oOIPresent;
     }
+    
 
     #endregion
 }
