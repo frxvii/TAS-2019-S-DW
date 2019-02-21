1bdb9d6ff5bb52f99bdfd866e6df42058d63fd3b
frxvii <frxvii@gmail.com>
2019-02-20 21:07:43 -0500

    wk4 start

---
+++ week4/Assets/Script/CrossProductCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossProductCalculator : MonoBehaviour
+{
+    public Vector3 vectorA;
+    public Vector3 vectorB;
+
+    public Vector3 outputvectorACrossB;
+    public Vector3 outputvectorBCrossA;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        outputvectorACrossB = Vector3.Cross(vectorA, vectorB);
+        outputvectorBCrossA = Vector3.Cross(vectorB, vectorA);
+        
+    }
+}
---
+++ week4/Assets/Script/MeshWriter.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+
+public class MeshWriter : MonoBehaviour
+{
+    #region Internal References
+    private MeshRenderer _myMR;
+    private MeshFilter _myMF;
+    #endregion
+
+    #region Data
+    private Mesh _myMesh;
+    public Vector3[] _verts;
+    private int[] _tris;
+    private Vector3[] _normals;
+    private Vector2[] _uVs;
+    #endregion
+
+
+
+    void Awake()
+    {
+        _myMR = GetComponent<MeshRenderer>();
+        _myMF = GetComponent<MeshFilter>();
+    }
+
+    private void Start()
+    {
+        _ComputeMesh();
+        _ApplyMesh();
+    }
+
+    private void _ComputeMesh()
+    {
+        _myMesh = new Mesh();
+
+        _verts = new Vector3[6];
+        _verts[0] = new Vector3(0,0,0);
+        _verts[1] = new Vector3(1,0,0); //* (Mathf.Sin(Time.time) + 1);
+        _verts[2] = new Vector3(0,1,0); //* (Mathf.Cos(Time.time) + 1);
+        _verts[3] = new Vector3(1,0,0);
+        _verts[4] = new Vector3(0,1,0);
+        _verts[5] = new Vector3(1,1,0);
+
+        _tris = new int[6];
+        _tris[0] = 0;
+        _tris[1] = 2;
+        _tris[2] = 1;
+        _tris[3] = 3;
+        _tris[4] = 4;
+        _tris[5] = 5;
+
+        _normals = new Vector3[6];
+        _normals[0] = -Vector3.forward;
+        _normals[1] = -Vector3.forward;
+        _normals[2] = -Vector3.forward;
+        _normals[3] = -Vector3.forward;
+        _normals[4] = -Vector3.forward;
+        _normals[5] = -Vector3.forward;
+
+        _uVs = new Vector2[6];
+        _uVs[0] = new Vector2(0, 0);
+        _uVs[1] = new Vector2(1, 0);
+        _uVs[2] = new Vector2(0, 1);
+        _uVs[3] = new Vector2(1, 0);
+        _uVs[4] = new Vector2(0, 1);
+        _uVs[5] = new Vector2(1, 1);
+    }
+
+    private void _RandomUVs()
+    {
+        for (int i = 0; i < _normals.Length; i++)
+        {
+            _normals[i] = Vector3.Normalize(Random.insideUnitSphere);
+        }
+    }
+
+    private void _ApplyMesh()
+    {
+        _myMesh.vertices = _verts;
+        _myMesh.triangles = _tris;
+        _myMesh.normals = _normals;
+        _myMesh.uv = _uVs;
+
+        _myMF.mesh = _myMesh;
+    }
+
+    private void Update()
+    {
+        //_verts[0] = Vector3.zero;
+        //_verts[1] = Vector3.right * (Mathf.Sin(Time.time) + 3);
+        //_verts[2] = Vector3.up * (Mathf.Cos(Time.time) + 3);
+
+        //_RandomUVs();
+
+        //_ApplyMesh();
+    }
+}
